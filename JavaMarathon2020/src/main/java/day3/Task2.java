b70935438f72cfd707cba4877edcecc8b01fed2e
Antorux <andrey-spam@mail.ru>
2024-09-30 23:24:30 +0300

    mini change

--- JavaMarathon2020/src/main/java/day3/Task2.java
+++ JavaMarathon2020/src/main/java/day3/Task2.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double x,y;
 
+        while(true) {
+            x = scan.nextDouble();
+            y = scan.nextDouble();
+
+            if (y == 0) {
+                break;
+            }
+
+            System.out.println(x / y);
+        }
     }
 }
+
