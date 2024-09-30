7c7817243caa7f0d5fcf8424c9449a1aea46f750
Antorux <andrey-spam@mail.ru>
2024-09-30 22:01:53 +0300

    1Day Complete

--- JavaMarathon2020/src/main/java/day1/Task6.java
+++ JavaMarathon2020/src/main/java/day1/Task6.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 3;
+        int i = 1;
+        while (i <= 10){
+            System.out.println(i + " * " + k + " = " + k * i);
+            i++;
+        }
     }
 }
